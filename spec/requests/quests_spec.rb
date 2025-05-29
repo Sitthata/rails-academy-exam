83678527b72abb50e9fe07888e6d52cca60176f8
Pattaradanai Srichon <aprils96950@gmail.com>
2025-05-29 09:27:58 +0700

    test:add quests request test

--- spec/requests/quests_spec.rb
+++ spec/requests/quests_spec.rb
@@ -3,23 +3,15 @@ require 'rails_helper'
 RSpec.describe "Quests", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/quests/index"
+      get root_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /new" do
     it "returns http success" do
-      get "/quests/new"
+      get new_quest_path
       expect(response).to have_http_status(:success)
     end
   end
-
-  describe "GET /create" do
-    it "returns http success" do
-      get "/quests/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
