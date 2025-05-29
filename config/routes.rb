506d13a4fab89b97a2844760f2066e8a75d14647
Pattaradanai Srichon <aprils96950@gmail.com>
2025-05-29 10:58:51 +0700

    feat:add brag document page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "quests#index"
   resources :quests
+  get "brag_documents", to: "page#index", as: :brag_documents
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
