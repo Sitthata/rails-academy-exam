35e88fb8bae9839a43380035451cd71bc47329c3
Pattaradanai Srichon <aprils96950@gmail.com>
2025-05-29 10:20:57 +0700

    e2e:add system test for creating new quest

--- spec/system/quests_spec.rb
+++ spec/system/quests_spec.rb
@@ -10,4 +10,36 @@ describe "Quests", type: :system do
       expect(page).to have_content("My Academy Quest")
     end
   end
+
+  context "when creating a new quest" do
+    before do
+      visit root_path
+    end
+
+    it "allows the user to create a new quest" do
+      click_on_new_quest_button
+      should_see_new_quest_page
+      create_new_quest("Added Test Quest")
+      should_see_the_added_quest("Added Test Quest")
+    end
+  end
+end
+
+def click_on_new_quest_button
+  click_link id: "new-quest-button"
+end
+
+def should_see_new_quest_page
+  expect(page).to have_content("New Quest")
+  expect(page).to have_current_path(new_quest_path)
+end
+
+def create_new_quest(name)
+  fill_in id: "quest-name-field", with: "Added Test Quest"
+  click_button id: "add-quest-button"
+  sleep 0.5
+end
+
+def should_see_the_added_quest(name)
+  expect(page).to have_content(name)
 end
